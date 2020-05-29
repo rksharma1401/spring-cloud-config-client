143bbf831b4ec1add93a966241c425461c9fd942
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-05-29 20:23:07 +0530

    ledger

--- src/main/java/learn/springcloud/configclient/LedgerController.java
+++ src/main/java/learn/springcloud/configclient/LedgerController.java
@@ -1,5 +1,4 @@
 package learn.springcloud.configclient;
- 
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,26 +6,43 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import learn.springcloud.configclient.dao.LedgerRepo;
+import learn.springcloud.configclient.model.Category;
 import learn.springcloud.configclient.model.Ledger;
- 
 
 @Controller
+@RestController
 public class LedgerController {
     @Autowired
-    LedgerRepo LedgerRepoImpl;
+    LedgerRepo ledgerRepoImpl;
 
     @RequestMapping("/ledgerList")
     public String getCategoryList(Model m) {
-        Iterable<Ledger> iterator = LedgerRepoImpl.findAll();
-       
-        List<Ledger> lst =  new ArrayList<>();
-        iterator.forEach(e->lst.add(e));
+        Iterable<Ledger> iterator = ledgerRepoImpl.findAll();
+
+        List<Ledger> lst = new ArrayList<>();
+        iterator.forEach(e -> lst.add(e));
 
         m.addAttribute("ledgerList", lst);
-        // name of view 
+        // name of view
         return "ledgerList";
+    }
+
+    @PostMapping("/addLedger")
+    public String addLedger(@RequestParam Long categoryId, @RequestParam Long amount) {
+        Ledger entity = new Ledger();
+        entity.setAmount(amount);
+        Category category=new Category();
+        category.setId(categoryId);
+        entity.setCategory(category);
+        entity=  ledgerRepoImpl.save(entity);
+        
+        // name of view 
+        return "success with Id : " +entity.getId();
     }
 }
