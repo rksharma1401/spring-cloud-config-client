c55344d10669494d438de660f87edd65bd161dcb
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-05-29 20:27:18 +0530

    ledger

--- src/main/java/learn/springcloud/configclient/LedgerController.java
+++ src/main/java/learn/springcloud/configclient/LedgerController.java
@@ -6,17 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import learn.springcloud.configclient.dao.LedgerRepo;
-import learn.springcloud.configclient.model.Category;
 import learn.springcloud.configclient.model.Ledger;
 
-@Controller
-@RestController
+@Controller 
 public class LedgerController {
     @Autowired
     LedgerRepo ledgerRepoImpl;
@@ -32,17 +27,5 @@ public class LedgerController {
         // name of view
         return "ledgerList";
     }
-
-    @PostMapping("/addLedger")
-    public String addLedger(@RequestParam Long categoryId, @RequestParam Long amount) {
-        Ledger entity = new Ledger();
-        entity.setAmount(amount);
-        Category category=new Category();
-        category.setId(categoryId);
-        entity.setCategory(category);
-        entity=  ledgerRepoImpl.save(entity);
-        
-        // name of view 
-        return "success with Id : " +entity.getId();
-    }
+ 
 }
---
+++ src/main/java/learn/springcloud/configclient/restController/LedgerRest.java
@@ -0,0 +1,31 @@
+package learn.springcloud.configclient.restController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import learn.springcloud.configclient.dao.LedgerRepo;
+import learn.springcloud.configclient.model.Category;
+import learn.springcloud.configclient.model.Ledger;
+
+@RestController
+public class LedgerRest {
+    
+    @Autowired
+    LedgerRepo ledgerRepoImpl;
+
+    @PostMapping("/addLedger")
+    public String addLedger(@RequestParam Long categoryId, @RequestParam Long amount) {
+        Ledger entity = new Ledger();
+        entity.setAmount(amount);
+        Category category=new Category();
+        category.setId(categoryId);
+        entity.setCategory(category);
+        entity=  ledgerRepoImpl.save(entity);
+        
+        // name of view 
+        return "success with Id : " +entity.getId();
+    }
+    
+}
