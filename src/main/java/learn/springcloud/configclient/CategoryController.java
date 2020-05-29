4cce5dfee46c5c82d2a4f055dd949b04ddb7b9aa
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-05-29 15:05:57 +0530

    postgress

---
+++ src/main/java/learn/springcloud/configclient/CategoryController.java
@@ -0,0 +1,30 @@
+package learn.springcloud.configclient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import learn.springcloud.configclient.dao.CategoryRepo;
+import learn.springcloud.configclient.model.Category;
+
+@Controller
+public class CategoryController {
+    @Autowired
+    CategoryRepo categoryRepoImpl;
+
+    @RequestMapping("/categoryList")
+    public String getCategoryList(Model m) {
+        Iterable<Category> iterator = categoryRepoImpl.findAll();
+       
+        List<Category> lst =  new ArrayList<>();
+        iterator.forEach(e->lst.add(e));
+
+        m.addAttribute("categoryList", lst);
+        // name of view 
+        return "categoryList";
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE category(
   id INTEGER  PRIMARY KEY,
-  category_name  VARCHAR(500) 
+  category_name  VARCHAR(500) ,
   CONSTRAINT category_categoryName_unique UNIQUE (category_name)
 ); 
