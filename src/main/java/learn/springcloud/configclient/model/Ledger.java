24cb041f16bac38bde20a1ff620d1695daa75bb1
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-05-29 20:33:16 +0530

    ledger

--- src/main/java/learn/springcloud/configclient/model/Ledger.java
+++ src/main/java/learn/springcloud/configclient/model/Ledger.java
@@ -1,12 +1,14 @@
 package learn.springcloud.configclient.model;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Ledger {
 
-    @Id
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     
     @ManyToOne
