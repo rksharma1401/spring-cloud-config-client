a2858567dacbbfe2b73c2610b8ea3577b543394e
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-05-29 14:43:14 +0530

    postgress

--- src/main/java/learn/springcloud/configclient/dao/CategoryRepo.java
+++ src/main/java/learn/springcloud/configclient/dao/CategoryRepo.java
@@ -1,9 +1,14 @@
-package learn.springcloud.configclient.dao; 
+package learn.springcloud.configclient.dao;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+
+import learn.springcloud.configclient.model.Category;
 
 public interface CategoryRepo extends CrudRepository<Category, Long> {
 
-  List<Customer> findByLastName(String lastName);
+  List<Category> findByCategoryName(String categoryName);
 
-  Customer findById(long id);
+  Category findById(long id);
 }
