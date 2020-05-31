a9f253fbd81ed3ccaf315221c0a3bd03ea9beb64
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-05-31 19:37:00 +0530

    path

--- src/main/java/learn/springcloud/configclient/LedgerController.java
+++ src/main/java/learn/springcloud/configclient/LedgerController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import learn.springcloud.configclient.dao.LedgerRepo;
 import learn.springcloud.configclient.model.Ledger;
 
-@Controller("/ledger") 
+@Controller 
 public class LedgerController {
     @Autowired
     LedgerRepo ledgerRepoImpl;
 
-    @RequestMapping("/list")
+    @RequestMapping("/ledger/list")
     public String getCategoryList(Model m) {
         Iterable<Ledger> iterator = ledgerRepoImpl.findAll();
 
