1353b602c8230b0e05118aa4a33fdd293a925022
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-06-14 19:50:31 +0530

    delete

--- src/main/java/learn/springcloud/configclient/restController/LedgerRest.java
+++ src/main/java/learn/springcloud/configclient/restController/LedgerRest.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +34,7 @@ public class LedgerRest {
         return "success with Id : " +entity.getId();
     }
 
-    @PostMapping("deleteLedger")
+    @DeleteMapping("deleteLedger")
     public String deleteLedger(@NotNull @RequestParam Long id) { 
          ledgerRepoImpl.deleteById(id); 
         return "success with Id : " +id;
