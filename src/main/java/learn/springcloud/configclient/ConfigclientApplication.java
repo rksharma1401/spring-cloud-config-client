c7696895fcbd97cf6e395c76089d51e54b4b3947
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-06-21 19:19:42 +0530

    feignclient eureka working

--- src/main/java/learn/springcloud/configclient/ConfigclientApplication.java
+++ src/main/java/learn/springcloud/configclient/ConfigclientApplication.java
@@ -1,5 +1,6 @@
 package learn.springcloud.configclient;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,29 +10,54 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
+import org.springframework.web.client.RestTemplate;
+
+import learn.springcloud.configclient.feignclients.*;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient 
+@EnableFeignClients
 public class ConfigclientApplication {
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
 	}
 
-}
+} 
+
+
 @RestController
 class ServiceInstanceRestController {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
-
+	
+	@Autowired 
+	private DateClient dateClient;
+	// client to see if app registered or not
 	@RequestMapping("/service-instances/{applicationName}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(
 			@PathVariable String applicationName) {
 		return this.discoveryClient.getInstances(applicationName);
+	}
+
+
+	@RequestMapping("/getDate")
+	public String getDate() {
+		return new Date().toString();
+	}
+
+	@RequestMapping("/getDateEureka")
+	public String getDateMi() {   
+		return dateClient.getDate();
 	}
 }
