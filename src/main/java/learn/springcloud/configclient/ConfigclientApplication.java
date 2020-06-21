84a45e9873dea922b3fb9e55f2a1b59309559f98
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-06-21 13:38:19 +0530

    add changes

--- src/main/java/learn/springcloud/configclient/ConfigclientApplication.java
+++ src/main/java/learn/springcloud/configclient/ConfigclientApplication.java
@@ -8,12 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient 
 public class ConfigclientApplication {
 
 	public static void main(String[] args) {
