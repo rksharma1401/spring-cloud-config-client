8e1cd8d397fc0588fc8885bc8b71ac2bfb460f84
Ravi <Ravi@DESKTOP-VRQEBDA>
2020-07-05 16:02:54 +0530

    add eureka urls

--- src/main/java/learn/springcloud/configclient/controller/PersonController.java
+++ src/main/java/learn/springcloud/configclient/controller/PersonController.java
@@ -3,51 +3,41 @@ package learn.springcloud.configclient.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import learn.springcloud.configclient.config.WebClientImpl;
-import learn.springcloud.configclient.feignclients.CurrentAppClient;
+import learn.springcloud.configclient.feignclients.WebservicesEureakaClient;
 import learn.springcloud.configclient.model.Person;
-import reactor.core.publisher.Flux;
- 
+
 @Controller
 public class PersonController {
 
-    
     @Autowired
     private WebClientImpl webClientImpl;
- 
+
     @Autowired
-    private CurrentAppClient userClient;
-    
-    
+    private WebservicesEureakaClient webservicesEureakaClient;
+
     @RequestMapping("/usersList")
-    public String getUserList(Model m,@Autowired Authentication authentication) {
-        List<Person> lst = null;
-         try {
-            Flux<Person> flux = webClientImpl.getWebClient()
-                .get()
-                .uri(
-                "/pc/getAllPersons")
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).retrieve() 
-                .bodyToFlux(Person.class);
-                lst = flux.collectList().block();  
-        } catch (Exception e) { 
-            lst = userClient.getUserList();
-        }
-        
-
-        
-
+    public String getUserList(Model m, @Autowired Authentication authentication) {
+        List<Person> lst = webservicesEureakaClient.getPersonList();
+        /*
+         * try { Flux<Person> flux = webClientImpl.getWebClient() .get() .uri(
+         * "/pc/getAllPersons") .header(HttpHeaders.CONTENT_TYPE,
+         * MediaType.APPLICATION_JSON_VALUE).retrieve() .bodyToFlux(Person.class); lst =
+         * flux.collectList().block(); } catch (Exception e) { lst =
+         * userClient.getUserList(); }
+         */
 
         m.addAttribute("personString", lst.toString());
         m.addAttribute("personList", lst);
-        m.addAttribute("userName",authentication.getName());
+        if (authentication != null)
+            m.addAttribute("userName", authentication.getName());
+        else
+            m.addAttribute("userName", "Guest");
         // name of view
         return "usersList";
     }
--- src/main/java/learn/springcloud/configclient/feignclients/WebservicesEureakaClient.java
+++ src/main/java/learn/springcloud/configclient/feignclients/WebservicesEureakaClient.java
@@ -1,19 +1,33 @@
 package learn.springcloud.configclient.feignclients;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-@FeignClient(name="WEB-SERVICE-EUREKA")//fallback = WebservicesEureakaFeignClientFallback.class
+
+import learn.springcloud.configclient.model.Person;
+@FeignClient(name="WEB-SERVICE-EUREKA" ,fallback = WebservicesEureakaFeignClientFallback.class)
 public interface WebservicesEureakaClient {
     @RequestMapping(method = RequestMethod.GET, value = "/getDate")
     public String getDate();
+    @RequestMapping(method = RequestMethod.GET, value = "/getPersonList")
+	public List<Person> getPersonList();
 }
 
-/* @Component
+  @Component
 class WebservicesEureakaFeignClientFallback implements WebservicesEureakaClient{
     @Override
     public String getDate() { 
         return "No Date from WEB-SERVICE-EUREKA";
     }
-} */
+
+    @Override
+    public List<Person> getPersonList() { 
+        List<Person> list= new ArrayList<Person>();
+        list.add(new Person("BACKUP","PERSON",0,"WebservicesEureakaFeignClientFallback"));
+        return list;
+    }
+} 
